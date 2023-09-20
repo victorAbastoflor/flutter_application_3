6e8fb0960d46d47638852659ca94c764f8f5fb35
victorAbastoflor <ayv1009450@est.univalle.edu>
2023-09-19 23:11:59 -0400

    Agregar usuario,actualzar usuarios y eliminar usuarios con coneccion a firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 //importaciones de firebase
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:flutter_application_3/pages/edit_user_page.dart';
 import 'firebase_options.dart';
+
+//pages
+import 'pages/add_user_page.dart';
 import 'pages/home_page.dart';
+//import 'pages/edit_user_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,9 +22,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Material App',
-      home: Home(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Home(),
+        '/add': (context) => const AddUserPage(),
+        '/edit': (context) => const EditUserPage(),
+      },
     );
   }
 }
--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,13 +1,60 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//instancia del firestore
 FirebaseFirestore db = FirebaseFirestore.instance;
-//funcion
+//funcion leer
 Future<List> getPersona() async {
   List person = [];
   CollectionReference collectionReferencePersona = db.collection('Persona');
   QuerySnapshot queryPersona = await collectionReferencePersona.get();
   queryPersona.docs.forEach((documento) {
-    person.add(documento.data());
+    final Map<String, dynamic> data = documento.data() as Map<String, dynamic>;
+    final persona = {
+      "nombre": data['nombre'],
+      "ApellidoPaterno": data['ApellidoPaterno'],
+      "ApellidoMaterno": data['ApellidoMaterno'],
+      "CarnetIdentidad": data['CarnetIdentidad'],
+      "CorreoElectronico": data['CorreoElectronico'],
+      "Genero": data['Genero'],
+      "uid": documento.id,
+    };
+    person.add(persona);
   });
   return person;
+}
+
+// Guardar un user en base de datos
+Future<void> addPeople(String name, String lastname, String last2name,
+    String nit, String correo, String genero) async {
+  await db.collection("Persona").add({
+    "nombre": name,
+    "ApellidoPaterno": lastname,
+    "ApellidoMaterno": last2name,
+    "CarnetIdentidad": nit,
+    "CorreoElectronico": correo,
+    "Genero": genero,
+  });
+}
+
+// Actualizar un user en base de datos
+Future<void> updatePeople(String uid, String name, String lastname,
+    String last2name, String nit, String correo, String genero) async {
+  try {
+    await db.collection("Persona").doc(uid).update({
+      "nombre": name,
+      "ApellidoPaterno": lastname,
+      "ApellidoMaterno": last2name,
+      "CarnetIdentidad": nit,
+      "CorreoElectronico": correo,
+      "Genero": genero,
+    });
+    print("Usuario actualizado correctamente.");
+  } catch (error) {
+    print("Error al actualizar el usuario: $error");
+  }
+}
+
+// Borrar datos de Firebase
+Future<void> deletePeople(String uid) async {
+  await db.collection("Persona").doc(uid).delete();
 }
